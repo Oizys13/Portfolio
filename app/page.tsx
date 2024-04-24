a3ecc21a61fdbb499da5eeaad38010c7cb08ca91
Oizys13 <brchreda@gmail.com>
2024-04-24 22:42:35 +0100

    Navigation Bar added

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
 <main className="h-full w-full">
-    <div className="flex flex-col gap-20">
+    <div className="flex flex-col gap-2">
       <Hero/>
     </div>
 </main>
