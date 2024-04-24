1768a84ecbc9aa3922673e2cfe5dcc4c2210c7e9
Oizys13 <brchreda@gmail.com>
2024-04-24 20:51:20 +0100

    Added stars effect

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -16,26 +16,20 @@ const HeroContent = () => {
 
             <motion.div 
             variants={slideInFromTop}
-            className='Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]'>
+            className='Welcome-box py-[8px] px-[15px] border border-[#7042f88b] opacity-[0.9]'>
                 <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
                 <h1 className='Welcome-text text-[13px]'>Fullstack Developer Portfolio</h1>
             </motion.div >
 
             <motion.div 
             variants={slideInFromLeft(0.5)}
-            className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'>
+            className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
+                <span>
                 Providing
-                <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to bg-cyan-500'> 
-                    The Best
-                </span>
+                <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to bg-cyan-500'> The Best </span>
                 Project Experience
+                </span>
             </motion.div>
-
-            <motion.p 
-            variants={slideInFromLeft(0.8)}
-            className="text-lg text-gray-400 my-5 max-w-[600p] h-[5px]">
-                Fullstack Software Engineer with experience in Websites and Software Developement        
-            </motion.p>
 
             <motion.p 
             variants={slideInFromLeft(0.8)}
@@ -47,11 +41,18 @@ const HeroContent = () => {
             variants={slideInFromLeft(0.8)}
             className="text-lg text-gray-400 my-5 max-w-[600p] h-[5px]">
                 Machine Learning Engineer
+
+            </motion.p>
+
+            <motion.p 
+            variants={slideInFromLeft(0.8)}
+            className="text-lg text-gray-400 my-5 max-w-[600p] h-[5px]">
+                Fullstack Software Engineer with experience in Websites and Software Developement        
             </motion.p>
 
             <motion.a
             variants={slideInFromLeft(1)}
-            className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]'
+            className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] mt-[20px]'
             >
                 Learn More
             </motion.a>
